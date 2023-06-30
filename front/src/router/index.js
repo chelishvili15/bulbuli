5c6ebe023d36408e26fbb064aef08898c6534ac4
chelishvili15 <chelo@redberry.ge>
2023-06-30 22:07:41 +0400

    boom

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../views/Home.vue'
-import DownloadPage from '@/views/DownloadPage.vue'
+import DownloadPage from '../views/DownloadPage.vue'
 
 const routes = [
   {path:'/', name: 'home', component: Home},
