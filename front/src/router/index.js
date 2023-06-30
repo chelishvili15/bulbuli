da53382955b9cc309a51142e9bef613d80517fc8
chelishvili15 <chelo@redberry.ge>
2023-06-30 22:16:01 +0400

    chore: add about

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../views/Home.vue'
 import DownloadPage from '../views/DownloadPage.vue'
+import About from '../views/About.vue'
 
 const routes = [
-  {path:'/', name: 'home', component: Home},
-  {path:'/download/:path*', name: 'about', component: DownloadPage}
+  {path:'/home', name: 'home', component: Home},
+  {path:'/about', name: 'about', component: About},
+  {path:'/download/:path*', name: 'download-page', component: DownloadPage}
 ]
 
 const router = createRouter({
---
+++ front/src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-2xl text-red-700">
+    About
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
