ca1bdfaee8e9faaafe126a861a6b94507357304d
chelishvili15 <chelo@redberry.ge>
2023-07-03 14:44:12 +0400

    feat: add axios and send request from site

--- front/src/main.js
+++ front/src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
+// import axios from 'axios'
 import router from './router'
 import App from './App.vue'
 
 createApp(App)
+// .use(VueAxios, axios) 
 .use(router)
 .mount('#app')
--- front/src/views/DownloadPage.vue
+++ front/src/views/DownloadPage.vue
@@ -1,19 +1,57 @@
 <template>
-  <div class="border-4 px-20 py-12 absolute left-1/2 -translate-x-1/2 top-10 rounded">
-    <button class="bg-blue-500 px-8 py-3 cursor-pointer rounded-xl text-gray-100 hover:bg-blue-700 hover:text-white">Download</button>
+  <div class="flex flex-col justify-center items-center p-10 gap-10 bg-red-50">
+    <div
+      class="border-4 px-20 py-12 rounded"
+    >
+      <button
+        class="bg-blue-500 px-8 py-3 cursor-pointer rounded-xl text-gray-100 hover:bg-blue-700 hover:text-white"
+        @click="buttonClicked"
+      >
+        Download
+      </button>
+    </div>
+    <div>
+      <span class="font-semibold">Title: </span>
+      <span>{{ title?.replace('[OFFICIAL VIDEO]', '') }}</span>
+    </div>
+
+    <iframe :src="src" height="90px" width="40px"></iframe>
   </div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
-import { useRouter, useRoute } from 'vue-router'
+import { onMounted, ref } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import axios from "axios";
 
-onMounted(() => {
-  const route = useRoute()
-  console.log(route.params.path[0])
+const link = ref({})
+const title = ref('')
+const src = ref('')
+const id = ref('XGcX5wopq3M')
+
+onMounted(async() => {
+  const route = useRoute();
+  id.value = route.params.path[0]
+
+  const options = {
+    method: "GET",
+    url: "https://youtube-mp36.p.rapidapi.com/dl",
+    params: { id: id.value },
+    headers: {
+      "X-RapidAPI-Key": "69f56c9d5dmsh47a93b89b474344p121e24jsnec66aa328a1e",
+      "X-RapidAPI-Host": "youtube-mp36.p.rapidapi.com",
+    },
+  }
+
+  const response = await axios.request(options);
+  
+  title.value = response.data.title
+  link.value = response.data.link
 })
+
+const buttonClicked = () => {
+  src.value = link.value
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
